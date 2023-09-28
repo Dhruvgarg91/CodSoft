609453e121892b963ddc3fa3e7a669011df96acf
Dhruvgarg91 <dhruv.g4445@gmail.com>
2023-09-28 13:22:25 +0530

    Updated

--- WordWeaver/server.js
+++ WordWeaver/server.js
@@ -16,27 +16,27 @@ app.get('/editor', (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"));
 })
 
-// upload link
+
 app.post('/upload', (req, res) => {
     let file = req.files.image;
     let date = new Date();
-    // image name
+
     let imagename = date.getDate() + date.getTime() + file.name;
-    // image upload path
+
     let path = 'public/uploads/' + imagename;
 
-    // create upload
+
     file.mv(path, (err, result) => {
         if(err){
             throw err;
         } else{
-            // our image upload path
+
             res.json(`uploads/${imagename}`)
         }
     })
 })
 
-app.get("/:blog", (req, res) => {
+app.get("/:post", (req, res) => {
     res.sendFile(path.join(initial_path, "blog.html"));
 })
 
@@ -45,5 +45,5 @@ app.use((req, res) => {
 })
 
 app.listen("3000", () => {
-    console.log('listening......');
+    console.log('Server Started...');
 })
